625280e3ea98167a837a602c92eeef493ce80940
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2022-05-06 12:22:58 +0800

    utils impl

---
+++ utils/fileop.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// CheckExists checks if a file or directory exists
+func CheckExists(path string) (bool, int) {
+	abspath, err := filepath.Abs(path)
+	if err != nil {
+		panic(err)
+	}
+	fdinfo, err := os.Stat(abspath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, -1
+		}
+		return false, -1
+	}
+	if fdinfo.IsDir() {
+		return true, 1
+	}
+	return true, 0
+}
+
+// MkdirP creates a directory and all its parents
+func MkdirP(path string) error {
+	if err := os.MkdirAll(path, 0755); err != nil {
+		return err
+	}
+	return nil
+}
+
+// XORStream uses XOR to transform data in a simple but not secure way
+func XORStream(key []byte, data []byte) []byte {
+	for i := 0; i < len(data); i++ {
+		data[i] = data[i] ^ key[0]
+	}
+	return data
+}
