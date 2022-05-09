705f60079cbb0997c87212b02da3f2d3891f3434
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2022-05-09 22:39:54 +0800

    build(build.sh):  update build script
    script only works for linux, mac os need a bit tweak

--- assets/build.sh
+++ assets/build.sh
@@ -9,7 +9,7 @@ BUILDRAND=$(openssl rand -hex 4)
 go mod download
 go install mvdan.cc/garble@latest
 
-garble -literals -tiny -seed=random build -o bin/xorer_${BUILDRAND} -trimpath -ldflags="-s -w -Xcommon.Version=$(git describe --always --dirty --long --tags) -Xcommon.EndUserID=$1 -Xcommon.EndUserNonce=$2 -Xcommon.EndUserPublicKey=$(cat ~/.ylic-root.pub) -Xcommon.EndUserLicenseType=T2" cmd/xorer.go
+garble -literals -tiny -seed=random build -o bin/xorer_${BUILDRAND} -trimpath -ldflags="-s -w -Xcommon.Version=$(git describe --always --dirty --long --tags) -Xcommon.EndUserID=$1 -Xcommon.EndUserNonce=$2 -Xcommon.EndUserPublicKey=$(cat ~/.ylic-root.pub|base64 -w 0) -Xcommon.EndUserLicenseType=T2" cmd/xorer.go
 
-garble -literals -tiny -seed=random build -o bin/clicker_${BUILDRAND} -trimpath -ldflags="-s -w -Xcommon.Version=$(git describe --always --dirty --tags) -Xcommon.EndUserID=$1 -Xcommon.EndUserNonce=$2 -Xcommon.EndUserPublicKey=$(cat ~/.ylic-root.pub) -Xcommon.EndUserLicenseType=T2" cmd/clicker.go
+garble -literals -tiny -seed=random build -o bin/clicker_${BUILDRAND} -trimpath -ldflags="-s -w -Xcommon.Version=$(git describe --always --dirty --tags) -Xcommon.EndUserID=$1 -Xcommon.EndUserNonce=$2 -Xcommon.EndUserPublicKey=$(cat ~/.ylic-root.pub|base64 -w 0) -Xcommon.EndUserLicenseType=T2" cmd/clicker.go
 
