91cfa480c8e0d68f749ad2c838acf22ddb58d895
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2022-05-10 01:31:28 +0800

    revert(global): cleanup fixed license

--- common/global.go
+++ common/global.go
@@ -10,7 +10,7 @@ var GlobalTemporaryStorage string
 var VERSION string
 
 // SOFTWARE LICENSE USAGE
-var EndUserLicenseType string = "T2"
-var EndUserID string = "nsf-wangdi-securities"
-var EndUserNonce string = "T2|2ee348cf-37aa-4096-becf-a7c3043f4b5d"
-var LicensePublicKey string = "MCowBQYDK2VwAyEAaukyZta4v49nWq4hBA85zsdp6+dnvQBpr94OLBUBUK0="
+var EndUserLicenseType string
+var EndUserID string
+var EndUserNonce string
+var LicensePublicKey string
